5a265bec262fba3e32b0641a7c65e04b54b5f3e6
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-11-08 15:10:43 +0000

    Add routing

--- src/app.py
+++ src/app.py
@@ -2,15 +2,27 @@ import os.path
 import time
 
 import dash
+import flask
 import views.layout as layout
+
+server = flask.Flask(__name__)
+
+
+@server.route('/')
+def index():
+    return 'Hello, go to dash/'
+
 
 external_stylesheets = [
     '//stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+dash_app = dash.Dash(__name__,
+                     server=server,
+                     external_stylesheets=external_stylesheets,
+                     routes_pathname_prefix='/dash/')
 
-app.title = "RSE Dashboard"
-layout.set_layout(app)
+dash_app.title = "RSE Dashboard"
+layout.set_layout(dash_app)
 
 if __name__ == '__main__':
-    app.run_server(debug=False, port=8050, host="0.0.0.0")
+    dash_app.run_server(debug=False, port=8050, host="0.0.0.0")
