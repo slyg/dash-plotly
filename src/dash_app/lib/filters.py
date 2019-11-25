3eacd3e20035ed5dcda2f4c0fe4af93b57a91ced
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-11-25 11:11:33 +0000

    Improve filters selector

--- src/dash_app/lib/filters.py
+++ src/dash_app/lib/filters.py
@@ -1,22 +1,21 @@
-def select(df, pipeline_type, project):
-
-    if pipeline_type == 'nightly':
-        df = df[df['job_name'].str.contains(
-            "HMCTS_Nightly") == True]
-    elif pipeline_type == 'non-nightly':
-        df = df[df['job_name'].str.contains(
-            "HMCTS_Nightly") == False]
-    else:
-        df = df
-
-    if project == 'all':
-        return df
-    else:
-        return df[df['job_name'].str.contains(project) == True]
-
-
 def select_project(df, project):
     if project == 'all':
         return df
     else:
         return df[df['job_name'].str.contains(project) == True]
+
+
+nightly_flag = "HMCTS_Nightly"
+
+
+def select_type(df, pipeline_type):
+    if pipeline_type == 'nightly':
+        return df[df['job_name'].str.contains(nightly_flag) == True]
+    elif pipeline_type == 'non-nightly':
+        return df[df['job_name'].str.contains(nightly_flag) == False]
+    else:
+        return df
+
+
+def select(df, pipeline_type, project):
+    return select_project(select_type(df, pipeline_type), project)
--- src/scripts/fetch-events.sh
+++ src/scripts/fetch-events.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e;
-echo "Fetching data";
+echo "Fetching data (can take a few minutes)";
 python datageneration/events_28d.py;
 python datageneration/events_180d.py;
