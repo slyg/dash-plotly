91af86a42beffb465a3d147c1ff69b0611238c39
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-11-25 16:11:33 +0000

    Slightly improve the data fetching perf

--- src/scripts/fetch-events.sh
+++ src/scripts/fetch-events.sh
@@ -2,5 +2,6 @@
 
 set -e;
 echo "Fetching data (can take a few minutes)";
-python datageneration/events_28d.py;
-python datageneration/events_180d.py;
+python datageneration/events_28d.py & \
+python datageneration/events_180d.py & \
+wait;
