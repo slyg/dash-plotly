f25bd71495d4e1207737bf483434a6013dfd08b9
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-12-02 11:57:03 +0000

    Reuse same layout for dash apps

--- src/dash_apps/components/layout.py
+++ src/dash_apps/components/layout.py
@@ -16,113 +16,123 @@ from dash_apps.components.navigation import getHeader, getNavigation
 from style.theme import BRAND, LIGHT_GREY, WHITE
 
 
+def layout(app, header, body):
+    return html.Div(
+        id="top",
+        children=[
+            html.Div(
+                className='container-fluid',
+                style={
+                    'background-color': LIGHT_GREY,
+                },
+                children=[
+                    html.Div(
+                        className='row my-3',
+                        children=[
+                            html.Div(className='col col-12',
+                                     children=header),
+                        ]),
+                    html.Div(
+                        className='row mt-5',
+                        children=body),
+                    html.Div(
+                        className='row mt-0 govuk-footer pt-0 pb-0',
+                        style={
+                            'background-color': WHITE,
+                        },
+                        children=[
+                            html.Div(className='col col-12 my-3 govuk-body-s',
+                                     children=[
+                                         html.Ul(className='govuk-footer__inline-list mb-0',
+                                                 children=[
+                                                     html.Li(className='govuk-footer__inline-list-item',
+                                                             children=[
+                                                                 html.A(href='/disclaimer',
+                                                                        className='govuk-footer__link',
+                                                                        children='Disclaimer'),
+                                                             ]),
+                                                     html.Li(className='govuk-footer__inline-list-item',
+                                                             children=[
+                                                                 html.A(href='https://github.com/hmcts/RSE-dashboard',
+                                                                        className='govuk-footer__link',
+                                                                        children='Source'),
+                                                             ])
+
+                                                 ]),
+                                     ]),
+                        ]),
+                ])
+
+        ])
+
+
 def set_pipelines_layout(app):
-    app.layout = html.Div(id="top", children=[
-        html.Div(
-            className='container-fluid',
-            style={
-                'background-color': LIGHT_GREY,
-            },
-            children=[
-                html.Div(
-                    className='row my-3',
-                    children=[
-                        html.Div(className='col col-12',
-                                 children=[getNavigation(app)]),
-                    ]),
-                html.Div(
-                    className='row mt-5',
-                    children=[
-                        html.Div(className='col col-12 col-xl-4 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(
-                                             color=BRAND, children=[dcc.Graph(id='build-status')])])
-                                 ]),
-                        html.Div(className='col col-12 col-xl-4 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(color=BRAND, children=[
-                                             dcc.Graph(id='build-status-14')])])
-                                 ]),
-                        html.Div(className='col col-12 col-xl-4 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(
-                                             color=BRAND, children=[dcc.Graph(id='nightly-vs-daily')])])
-                                 ]),
-                        html.Div(className='col col-12 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(
-                                             color=BRAND, children=[dcc.Graph(id='heatmap')])])
-                                 ]),
-                        html.Div(className='col col-12 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(
-                                             color=BRAND, children=[dcc.Graph(id='timeline-short')])])
-                                 ]),
-                        html.Div(className='col col-12 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(
-                                             color=BRAND, children=[dcc.Graph(id='most-failing')])])
-                                 ]),
-                        html.Div(className='col col-12 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(color=BRAND, children=[
-                                             dcc.Graph(id='failing-steps')])])
-                                 ]),
-                        html.Div(className='col col-12 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(color=BRAND, children=[
-                                             dcc.Graph(id='lengthy-pipelines')])])
-                                 ]),
-                        html.Div(className='col col-12 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(
-                                             color=BRAND, children=[dcc.Graph(id='duration')])])
-                                 ]),
-                        html.Div(className='col col-12 my-3',
-                                 children=[
-                                     html.Div(children=[
-                                         dcc.Loading(
-                                             color=BRAND, children=[dcc.Graph(id='timeline')])])
-                                 ]),
-                    ]),
-                html.Div(
-                    className='row mt-0 govuk-footer pt-0 pb-0',
-                    style={
-                        'background-color': WHITE,
-                    },
-                    children=[
-                        html.Div(className='col col-12 my-3 govuk-body-s',
-                                 children=[
-                                     html.Ul(className='govuk-footer__inline-list mb-0',
-                                             children=[
-                                                 html.Li(className='govuk-footer__inline-list-item',
-                                                         children=[
-                                                             html.A(href='/disclaimer',
-                                                                    className='govuk-footer__link',
-                                                                    children='Disclaimer'),
-                                                         ]),
-                                                 html.Li(className='govuk-footer__inline-list-item',
-                                                         children=[
-                                                             html.A(href='https://github.com/hmcts/RSE-dashboard',
-                                                                    className='govuk-footer__link',
-                                                                    children='Source'),
-                                                         ])
 
-                                             ]),
-                                 ]),
-                    ]),
-            ]),
+    header = [getNavigation(app)]
+    body = [
+        html.Div(className='col col-12 col-xl-4 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(
+                             color=BRAND, children=[dcc.Graph(id='build-status')])])
+                 ]),
+        html.Div(className='col col-12 col-xl-4 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(color=BRAND, children=[
+                             dcc.Graph(id='build-status-14')])])
+                 ]),
+        html.Div(className='col col-12 col-xl-4 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(
+                             color=BRAND, children=[dcc.Graph(id='nightly-vs-daily')])])
+                 ]),
+        html.Div(className='col col-12 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(
+                             color=BRAND, children=[dcc.Graph(id='heatmap')])])
+                 ]),
+        html.Div(className='col col-12 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(
+                             color=BRAND, children=[dcc.Graph(id='timeline-short')])])
+                 ]),
+        html.Div(className='col col-12 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(
+                             color=BRAND, children=[dcc.Graph(id='most-failing')])])
+                 ]),
+        html.Div(className='col col-12 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(color=BRAND, children=[
+                             dcc.Graph(id='failing-steps')])])
+                 ]),
+        html.Div(className='col col-12 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(color=BRAND, children=[
+                             dcc.Graph(id='lengthy-pipelines')])])
+                 ]),
+        html.Div(className='col col-12 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(
+                             color=BRAND, children=[dcc.Graph(id='duration')])])
+                 ]),
+        html.Div(className='col col-12 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(
+                             color=BRAND, children=[dcc.Graph(id='timeline')])])
+                 ]),
+    ]
 
-    ])
+    app.layout = layout(app, header, body)
 
     @app.callback(Output('build-status', 'figure'), [Input('pipeline-type', 'value'), Input('project', 'value')])
     def build_status_update(pipeline_type, project):
@@ -166,47 +176,30 @@ def set_pipelines_layout(app):
 
 
 def set_security_layout(app):
-    app.layout = html.Div(
-        id="top",
-        children=[
-            html.Div(
-                className='container-fluid',
-                style={
-                    'background-color': LIGHT_GREY,
-                },
-                children=[
-                    html.Div(
-                        className='row my-3',
-                        children=[
-                            html.Div(className='col col-12',
-                                     children=[getHeader(app)]),
-                        ]),
-                    html.Div(
-                        className='row mt-5',
-                        children=[
-                            html.Div(className='col col-12 col-xl-6 my-3',
-                                     children=[
-                                         html.Div(children=[
-                                             dcc.Loading(color=BRAND, children=[
-                                                 dcc.Graph(
-                                                         id='cve_suppressions_most_frequent',
-                                                         figure=cve_suppressions_most_frequent.get_fig()
-                                                         )
-                                             ])
-                                         ]),
-                                     ]),
-                            html.Div(className='col col-12 col-xl-6 my-3',
-                                     children=[
-                                         html.Div(children=[
-                                             dcc.Loading(color=BRAND, children=[
-                                                 dcc.Graph(
-                                                         id='cve_suppressions_per_team',
-                                                         figure=cve_suppressions_per_team.get_fig()
-                                                         )
-                                             ])
-                                         ]),
+    header = [getHeader(app)]
+    body = [
+        html.Div(className='col col-12 col-xl-6 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(color=BRAND, children=[
+                                     dcc.Graph(
+                                         id='cve_suppressions_most_frequent',
+                                         figure=cve_suppressions_most_frequent.get_fig()
+                                     )
                                      ])
-                        ])
-                ])
+                     ]),
+                 ]),
+        html.Div(className='col col-12 col-xl-6 my-3',
+                 children=[
+                     html.Div(children=[
+                         dcc.Loading(color=BRAND, children=[
+                                     dcc.Graph(
+                                         id='cve_suppressions_per_team',
+                                         figure=cve_suppressions_per_team.get_fig()
+                                     )
+                                     ])
+                     ]),
+                 ])
+    ]
 
-        ])
+    app.layout = layout(app, header, body)
