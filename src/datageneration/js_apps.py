83a276551f9ab7ff757c7d0e6408856bbab377b1
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-12-03 11:34:39 +0000

    Add js checks data fetching

---
+++ src/datageneration/js_apps.py
@@ -0,0 +1,71 @@
+import json
+import re
+import urllib
+import xml.etree.ElementTree as et
+from csv import DictWriter
+from os import environ
+
+import numpy as np
+import pandas as pd
+from requests import get
+
+GITHUB_TOKEN = environ['githubtoken']
+API_CODE_SEARCH_BASE = "https://api.github.com/search/code"
+API_ISSUES_SEARCH_BASE = "https://api.github.com/search/issues"
+
+OUTPUT_FILE = "reports/js-apps-snyk-report.csv"
+QUERY_ALL_JS = "org:hmcts+path:/+filename:package.json"
+QUERY_WITH_SNYK = "org:hmcts+path:/+filename:package.json+snyk"
+QUERY_WITH_NSP = "org:hmcts+path:/+filename:package.json+nsp"
+QUERY_DEPENDABOT = "org:hmcts+dependabot"
+
+
+def get_repos_for_code_search(query):
+    url = "{}?q={}".format(API_CODE_SEARCH_BASE, query)
+    data = get(url, headers={
+               "Authorization": "token {}".format(GITHUB_TOKEN)}).json()
+
+    def lens(item): return {'name': item['repository']['name']}
+    return map(lens, data['items'])
+
+
+def get_repos_for_issues_search(query):
+    url = "{}?q={}".format(API_ISSUES_SEARCH_BASE, query)
+    data = get(url, headers={
+               "Authorization": "token {}".format(GITHUB_TOKEN)}).json()
+
+    def lens(item): return {'name': item['repository_url'].rsplit('/', 1)[1]}
+    return map(lens, data['items'])
+
+
+js_projects = np.array(
+    list(get_repos_for_code_search(QUERY_ALL_JS)))
+js_projects_with_snyk = np.array(
+    list(get_repos_for_code_search(QUERY_WITH_SNYK)))
+js_projects_with_nsp = np.array(
+    list(get_repos_for_code_search(QUERY_WITH_NSP)))
+js_projects_with_dependabot = np.array(
+    list(get_repos_for_issues_search(QUERY_DEPENDABOT)))
+
+
+all_concatenated = np.concatenate((
+    js_projects,
+    js_projects_with_snyk,
+    js_projects_with_nsp,
+    js_projects_with_dependabot
+))
+
+all_projects_sorted = sorted([dict(t) for t in {tuple(d.items()) for d in all_concatenated}],
+                             key=lambda item: item['name'])
+
+rows = list(map(lambda item: {
+    'Reference': item['name'],
+    'Uses Snyk': 1 if (item in js_projects_with_snyk) else 0,
+    'Uses deprecated nsp': 1 if (item in js_projects_with_nsp) else 0,
+    'Uses dependabot': 1 if (item in js_projects_with_dependabot) else 0
+}, all_projects_sorted))
+
+df = pd.DataFrame.from_records(rows)
+
+df.to_csv('data/js_apps_sec_checks.csv',
+          sep=',', encoding='utf-8', index=False)
--- src/scripts/fetch.sh
+++ src/scripts/fetch.sh
@@ -5,4 +5,5 @@ echo "Fetching data (can take a few minutes)";
 python datageneration/cve_suppressions.py & \
 python datageneration/events_28d.py & \
 python datageneration/events_180d.py & \
+python datageneration/js_apps.py & \
 wait;
