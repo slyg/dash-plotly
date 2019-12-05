d6f62ab3d2a5dba40f1f97e1c6beedf3de86b077
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-12-05 11:37:13 +0000

    Fetch data sequentially
    As rate limit is reached

--- src/scripts/fetch.sh
+++ src/scripts/fetch.sh
@@ -5,7 +5,7 @@ echo "Fetching data (can take a few minutes)";
 python datageneration/cve_suppressions.py & \
 python datageneration/events_28d.py & \
 python datageneration/events_180d.py & \
-python datageneration/js_apps.py & \
-python datageneration/node_versions.py & \
-python datageneration/java_versions.py & \
 wait;
+python datageneration/js_apps.py;
+python datageneration/node_versions.py;
+python datageneration/java_versions.py;
